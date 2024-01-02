3c8553c4afa3531acf2b931c375716f5ddad51ed
AresRebatto <aresrebatt@gmail.com>
2024-01-02 12:29:45 +0100

    Create AppBar for ToDoList Page

--- lib/pages/ToDo.dart
+++ lib/pages/ToDo.dart
@@ -2,13 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:task_trek/pages/StandardWidgets.dart';
 import 'package:task_trek/Colori.dart';
 
-class ToDoPage extends StatelessWidget
-{
-  const ToDoPage({super.key});
 
+
+class ToDoPageState extends StatefulWidget
+{
+  const ToDoPageState({super.key});
+
+  @override
+
+  State<ToDoPageState> createState() => ToDoPage();
+}
+
+enum Pagina{
+  oggi,
+  domani
+}
+class ToDoPage extends State<ToDoPageState>
+{
+  Pagina statoPagina = Pagina.oggi;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: ToDoAppBar(),
       body: Stack(
         children: [
           Container(
@@ -27,4 +42,73 @@ class ToDoPage extends StatelessWidget
     );
   }
 
+  Widget Page()
+  {
+    if(statoPagina == Pagina.oggi)
+      return ListView(
+        children: [
+
+        ],
+      );
+    else
+      return ListView(
+        children: [
+
+        ],
+      );
+  }
+  PreferredSize ToDoAppBar(){
+    return PreferredSize(
+        preferredSize: const Size.fromHeight(90.0),
+        child: Container(
+            color: Colori.violet,
+            child: Container(
+              margin: const EdgeInsets.only(top: 50.0, left: 10.0),
+              alignment: Alignment.centerLeft,
+              child: Column(
+                children: [
+                  Text(
+                    "ToDo",
+                    style: TextStyle(
+                        color: Colori.white,
+                        fontSize: 22
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 12.0, left: 5),
+                        child:TextButton(
+                            onPressed: () {  },
+                            child: Text(
+                              "Oggi",
+                              style: TextStyle(
+                                  color: Colori.white,
+                                  fontSize: 15
+                              ),
+                            )
+                        ),
+                      ),Container(
+                        margin: const EdgeInsets.only(right: 20.0, left: 5),
+                        child:TextButton(
+
+                            onPressed: () {  },
+                            child: Text(
+                              "Domani",
+                              style: TextStyle(
+                                  color: Colori.white,
+                                  fontSize: 15
+                              ),
+                            )
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+        )
+
+    );
+}
 }
